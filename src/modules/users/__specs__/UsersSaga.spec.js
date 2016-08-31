1e4b85fc7de7e8d6c6a084028577607ea0ef8fad
Max Mikkola <mmik@fl804.local>
2016-08-31 15:23:38 +0300

    Added test for Usersaga failures

--- src/modules/users/__specs__/UsersSaga.spec.js
+++ src/modules/users/__specs__/UsersSaga.spec.js
@@ -4,7 +4,7 @@ import { call } from 'redux-saga/effects'
 
 import { get } from '@utils/request'
 import { requestUser as requestUserGenerator } from '../UsersSaga'
-import { addUser } from '../UsersState'
+import { addUser, requestUserFailed } from '../UsersState'
 
 describe('UsersSaga', () => {
   let iterator
@@ -17,10 +17,21 @@ describe('UsersSaga', () => {
     expect(iterator.next().value).to.deep.equal(call(get, 'http://api.randomuser.me/?inc=name,gender,location,picture&noinfo'))
   })
 
-  it('adds fetched user', () => {
+  it('calls ADD_USER with fetched user', () => {
     iterator.next() // Get the promised stuff
 
     const action = addUser()
     expect(iterator.next().value.PUT.action).to.deep.equal(action)
+  })
+
+  it('calls REQUEST_USER_FAILURE if thrown', () => {
+    const error = { message: 'test error' }
+    //Step into the try block by yielding the api call
+    iterator.next()
+
+    //Throw the iterator, and check catched action
+    const action = iterator.throw(error).value.PUT.action
+
+    expect(action).to.deep.equal(requestUserFailed(error))
   })
 })
